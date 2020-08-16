42080802c5149fb585efb1ba564d666b654e0606
takecap <iinumaleaguer@yahoo.co.jp>
2020-08-16 10:21:48 +0900

    README.mdを修正

--- src/ch01.py
+++ src/ch01.py
@@ -1,11 +1,8 @@
-import time
-def binary_search(sorted_list, item):
+def binarySearch(sorted_list, item):
   low, high = 0, len(sorted_list) - 1
   while low <= high:
     mid = (low + high) // 2
     guess = sorted_list[mid]
-    time.sleep(1)
-    print(low, mid, high)
     if guess > item:
       high = mid - 1
     elif guess < item:
