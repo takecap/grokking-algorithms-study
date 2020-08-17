97420f1f81a93ff519d433bc38054044a4ecba64
takecap <iinumaleaguer@yahoo.co.jp>
2020-08-17 11:22:27 +0900

    test01のbinarySearch関数のテストケースを追加

--- tests/test01.py
+++ tests/test01.py
@@ -3,7 +3,11 @@ from src import ch01
 
 class Ch01Tests(unittest.TestCase):
   def test_binary_search(self):
-    my_list = [0, 1, 2, 3, 5, 8]
-    expected = 4
-    actual = ch01.binarySearch(my_list, 5)
-    self.assertEqual(expected, actual)
+    test_patterns = [
+      ([0, 1, 2, 3, 5, 8], 5, 4),
+      ([0, 1, 2, 3, 5, 8], 10, None)
+    ]
+
+    for target_list, target_item, expected in test_patterns:
+      with self.subTest(sorted_list=target_list, item=target_item):
+        self.assertEqual(ch01.binarySearch(target_list, target_item), expected)
