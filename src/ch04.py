474fc6cce9336302cfb7f9dd2088119e228076fb
takecap <iinumaleaguer@yahoo.co.jp>
2020-08-20 08:34:08 +0900

    ch04のsum関数とquickSort関数を実装

---
+++ src/ch04.py
@@ -0,0 +1,15 @@
+def sum(arr):
+  if len(arr) == 0:
+    return 0
+  elif len(arr) == 1:
+    return arr[0]
+  else:
+    return arr[0] + sum(arr[1:])
+
+def quickSort(arr):
+  if len(arr) < 2:
+    return arr
+  pivot = arr[0]
+  small = [num for num in arr[1:] if num < pivot]
+  large = [num for num in arr[1:] if num >= pivot]
+  return quickSort(small) + [pivot] + quickSort(large)
--- tests/test04.py
+++ tests/test04.py
@@ -15,7 +15,8 @@ class Ch02Tests(unittest.TestCase):
   def test_quickSort(self):
     test_params = [
       ([4, 3, 2, 1, 0], [0, 1, 2, 3, 4]),
-      ([1, -3, 0.5, -0.2], [-3, -0.2, 0.5, 1])
+      ([1, -3, 0.5, -0.2], [-3, -0.2, 0.5, 1]),
+      ([3, 2, 1, 3, 2, 1], [1, 1, 2, 2, 3, 3])
     ]
 
     for target_list, expected in test_params:
