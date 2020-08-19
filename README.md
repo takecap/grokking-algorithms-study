64ab67c1bf4fefd2fe9c3eb08281817f6f38f8ac
takecap <iinumaleaguer@yahoo.co.jp>
2020-08-19 10:10:02 +0900

    sum関数とquickSort関数のテストを実装

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
 # grokking-algorithms-study
 
 ## 第１章 あれもこれもアルゴリズム
+- binarySearch関数
 
 ## 第２章 並べたり差し込んだり選んだり：ソート
+- findSmallest関数
+- selectionSort関数
 
 ## 第３章 同じ手順で何度でも：再帰
 
 ## 第４章 ちっちゃくしてから考えよう：クイックソート
+- sum関数
+- quickSort関数
---
+++ tests/test04.py
@@ -0,0 +1,23 @@
+import unittest
+from src import ch04
+
+class Ch02Tests(unittest.TestCase):
+  def test_sum(self):
+    test_params = [
+      ([0, 1, 2, 3, 4], 10),
+      ([1, -3, 0.5, -0.2], -1.7)
+    ]
+
+    for target_list, expected in test_params:
+      with self.subTest(arr=target_list):
+        self.assertEqual(ch04.sum(target_list), expected)
+  
+  def test_quickSort(self):
+    test_params = [
+      ([4, 3, 2, 1, 0], [0, 1, 2, 3, 4]),
+      ([1, -3, 0.5, -0.2], [-3, -0.2, 0.5, 1])
+    ]
+
+    for target_list, expected in test_params:
+      with self.subTest(arr=target_list):
+        self.assertEqual(ch04.quickSort(target_list), expected)
