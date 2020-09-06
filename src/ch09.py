456410733ffb38b3127d9878d989914d4b101648
takecap <iinumaleaguer@yahoo.co.jp>
2020-09-06 10:28:39 +0900

    ナップザック問題のセットアップを実装

--- src/ch09.py
+++ src/ch09.py
@@ -1,3 +1,8 @@
 def solveKnapsackProblem(items, capacity, unit=1):
-  results = set()
-  return results
+  max_factor = int(capacity / unit)
+  key_list = list(items.keys())
+  values = [[set() for i in range(max_factor + 1)] for j in range(len(key_list) + 1)]
+  return values[len(key_list)][max_factor]
+
+def evalValue(items, itemSet):
+  return sum([items[key][0] for key in itemSet])
