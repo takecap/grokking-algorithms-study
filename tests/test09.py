ab8cdbb6ca6a898d00bed3e55535a89deeedfbc2
takecap <iinumaleaguer@yahoo.co.jp>
2020-09-08 08:48:09 +0900

    ナップザック問題のテストケースを追加

--- tests/test09.py
+++ tests/test09.py
@@ -6,7 +6,15 @@ class Ch09Tests(unittest.TestCase):
     test_params = [
       ({'guitar': (1500, 1), 'stereo': (3000, 4), 'laptop': (2000, 3)}, 4, 1, {'guitar', 'laptop'}),
       ({'guitar': (1500, 1), 'stereo': (3000, 4), 'laptop': (2000, 3), 'iphone': (2000, 1)}, 4, 1, {'iphone', 'laptop'}),
-      ({'wm': (7, 0.5), 'gt': (6, 0.5), 'ng': (9, 1), 'bm': (9, 2), 'cc': (8, 0.5)}, 2, 0.5, {'wm', 'ng', 'cc'})
+      ({'wm': (7, 0.5), 'gt': (6, 0.5), 'ng': (9, 1), 'bm': (9, 2), 'cc': (8, 0.5)}, 2, 0.5, {'wm', 'ng', 'cc'}),
+      (
+        {'guitar': (1500, 1), 'stereo': (3000, 4), 'laptop': (2000, 3), 'iphone': (2000, 1), 'mp3': (1000, 1)},
+        4, 1, {'iphone', 'guitar', 'mp3'}
+      ),
+      (
+        {'water': (10, 3), 'book': (3, 1), 'food': (9, 2), 'jacket': (5, 2), 'camera': (6, 1)},
+        6, 1, {'food', 'camera', 'water'}
+      )
     ]
     
     for items, capa, unit, expected in test_params:
