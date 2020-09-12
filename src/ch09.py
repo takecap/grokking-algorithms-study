b40741134961a161b040cedc0742d6fff5b94113
takecap <iinumaleaguer@yahoo.co.jp>
2020-09-12 09:32:35 +0900

    最長部分列のソルバー関数の修正

--- src/ch09.py
+++ src/ch09.py
@@ -13,7 +13,6 @@ def solveKnapsackProblem(items, capacity, unit=1):
       if i >= weight and evalValue(items, item_set[j][i]) < evalValue(items, item_set[j][i - weight]) + value:
         item_set[j + 1][i] = copy.copy(item_set[j][i - weight])
         item_set[j + 1][i].add(key)
-#    print(item_set[j + 1])
   return item_set[len(key_list)][max_factor]
 
 def evalValue(items, itemSet):
@@ -22,24 +21,24 @@ def evalValue(items, itemSet):
 def solveLCSubsequence(string1, string2):
   str1 = string1.lower()
   str2 = string2.lower()
-  grid = [[0 for i in string1] for j in string2]
+  grid = [[0 for j in string2] for i in string1]
   for i, ch1 in enumerate(str1):
     for j, ch2 in enumerate(str2):
       if i == 0 and j == 0:
-        grid[j][i] = 1 if ch1 == ch2 else 0
+        grid[i][j] = 1 if ch1 == ch2 else 0
       elif j == 0:
         if ch1 == ch2:
-          grid[j][i] = 1
+          grid[i][j] = 1
         else:
-          grid[j][i] = grid[j][i - 1]
+          grid[i][j] = grid[i][j - 1]
       elif i == 0:
         if ch1 == ch2:
-          grid[j][i] = 1
+          grid[i][j] = 1
         else:
-          grid[j][i] = grid[j - 1][i]
+          grid[i][j] = grid[i][j - 1]
       else:
         if ch1 == ch2:
-          grid[j][i] = grid[j - 1][i - 1] + 1
+          grid[i][j] = grid[i - 1][j - 1] + 1
         else:
-          grid[j][i] = max(grid[j][i - 1], grid[j - 1][i])
-  return grid[j][i]
+          grid[i][j] = max(grid[i][j - 1], grid[i - 1][j])
+  return grid[i][j]
