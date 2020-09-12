e39b3ea7bfc75a301082830809bf35ff11f00098
takecap <iinumaleaguer@yahoo.co.jp>
2020-09-13 08:04:17 +0900

    最長共通部分列のDPの表に0行と0列を追加するよう修正

--- src/ch09.py
+++ src/ch09.py
@@ -21,24 +21,11 @@ def evalValue(items, itemSet):
 def solveLCSubsequence(string1, string2):
   str1 = string1.lower()
   str2 = string2.lower()
-  grid = [[0 for j in string2] for i in string1]
-  for i, ch1 in enumerate(str1):
-    for j, ch2 in enumerate(str2):
-      if i == 0 and j == 0:
-        grid[i][j] = 1 if ch1 == ch2 else 0
-      elif j == 0:
-        if ch1 == ch2:
-          grid[i][j] = 1
-        else:
-          grid[i][j] = grid[i][j - 1]
-      elif i == 0:
-        if ch1 == ch2:
-          grid[i][j] = 1
-        else:
-          grid[i][j] = grid[i][j - 1]
+  grid = [[0 for j in range(len(str2) + 1)] for i in range(len(str1) + 1)]
+  for i, ch1 in enumerate(str1, 1):
+    for j, ch2 in enumerate(str2, 1):
+      if ch1 == ch2:
+        grid[i][j] = grid[i - 1][j - 1] + 1
       else:
-        if ch1 == ch2:
-          grid[i][j] = grid[i - 1][j - 1] + 1
-        else:
-          grid[i][j] = max(grid[i][j - 1], grid[i - 1][j])
+        grid[i][j] = max(grid[i - 1][j], grid[i][j - 1])
   return grid[i][j]
